1244534a5bba7af4972661fbda65f58a95bd0667
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-23 20:44:16 -0300

    feat: providing default theme to global styles

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -12,4 +12,24 @@ export default createGlobalStyle`
     font-size: 62.5%;
   }
 
+  body {
+    font-family: ${({ theme }) => theme.font.family};
+    font-size: ${({ theme }) => theme.font.size.text.md};
+
+    background-color: ${({ theme }) => theme.colors.gray[50]};
+    color: ${({ theme }) => theme.colors.gray[900]};
+  }
+
+  h1 {
+    font-size: ${({ theme }) => theme.font.size.heading.lg};
+  }
+
+  h2 {
+    font-size: ${({ theme }) => theme.font.size.heading.md};
+  }
+
+  h3 {
+    font-size: ${({ theme }) => theme.font.size.heading.sm};
+  }
+
 `;
