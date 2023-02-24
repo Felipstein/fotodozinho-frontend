632a8a9b94304e72b6b908bdaf5dbfdfcd062989
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-23 21:33:41 -0300

    feat: added color to h1 in home page

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -4,4 +4,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  h1 strong {
+    color: ${({ theme }) => theme.colors.blue[500]};
+  }
 `;
