b0bcbca8fc698b2ea85727b0a8dc78e2ed5f3fb6
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-23 21:07:19 -0300

    feat: added dinamic element type to Text comp

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -1,14 +1,17 @@
+import { Slot } from '@radix-ui/react-slot';
 import { ReactNode } from 'react';
 
 interface TextProps {
+  asChild?: boolean;
   children: ReactNode;
 }
 
-export const Text: React.FC<TextProps> = ({ children }) => {
+export const Text: React.FC<TextProps> = ({ asChild = false, children }) => {
+  const Comp = asChild ? Slot : 'span';
 
   return (
-    <p>
+    <Comp>
       {children}
-    </p>
+    </Comp>
   );
 };
