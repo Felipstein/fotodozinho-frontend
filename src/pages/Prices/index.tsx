a9e227265571ef6ea4d55bcf1a12bdbd38e91ce6
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-04 17:41:33 -0300

    refactor: removed unused import

--- src/pages/Prices/index.tsx
+++ src/pages/Prices/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { Button } from '../../components/Button';
 import { Footer } from '../../components/Footer';
 import { Loading } from '../../components/Loading';
