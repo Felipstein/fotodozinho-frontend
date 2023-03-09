fc7b800659cd7fcbf5c1049b00d64f5ad2910393
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-08 22:18:59 -0300

    feat: added form validation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import { Button } from '../../components/Button';
 import { CheckBox } from '../../components/CheckBox';
 import { Footer } from '../../components/Footer';
@@ -11,8 +12,44 @@ import { TermsModal } from './components/modals/TermsModal';
 
 import * as S from './styles';
 
+interface FieldErrorFeedback {
+  fieldName: string;
+  feedback: string;
+}
+
 export const SignUp: React.FC = () => {
   const [isModalOpened, setIsModalOpened] = useState(false);
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+
+  const [fieldsErrors, setFieldsErrors] = useState<FieldErrorFeedback[]>([]);
+
+  function setError({ fieldName, feedback }: FieldErrorFeedback) {
+
+    setFieldsErrors(prevState => {
+      const index = prevState.findIndex(field => field.fieldName === fieldName);
+
+      if(index !== -1) {
+        prevState.splice(index, 1);
+      }
+
+      return [...prevState, { fieldName, feedback }];
+    });
+  }
+
+  function removeError(fieldName: string) {
+    setFieldsErrors(prevState => prevState.filter(field => field.fieldName !== fieldName));
+  }
+
+  function getErrorFeedback(fieldName: string) {
+    const fieldError = fieldsErrors.find(field => field.fieldName === fieldName);
+
+    return fieldError?.feedback;
+  }
 
   function handleCloseModal() {
     setIsModalOpened(false);
@@ -20,6 +57,60 @@ export const SignUp: React.FC = () => {
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
+  }
+
+  function handleNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    if(!value) {
+      setError({ fieldName: 'name', feedback: 'Nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
+
+    setName(value);
+  }
+
+  function handleEmailChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    if(!value) {
+      setError({ fieldName: 'email', feedback: 'E-mail é obrigatório' });
+    } else {
+      removeError('email');
+    }
+
+    setEmail(value);
+  }
+
+  function handlePhoneChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    setPhone(value);
+  }
+
+  function handlePasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    if(!value) {
+      setError({ fieldName: 'password', feedback: 'Senha é obrigatória' });
+    } else {
+      removeError('password');
+    }
+
+    setPassword(value);
+  }
+
+  function handleConfirmPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+
+    if(!value) {
+      setError({ fieldName: 'confirmPassword', feedback: 'Confirmar senha é obrigatória' });
+    } else {
+      removeError('confirmPassword');
+    }
+
+    setConfirmPassword(value);
   }
 
   return (
@@ -40,6 +131,9 @@ export const SignUp: React.FC = () => {
                 label='Nome'
                 name='name'
                 type='text'
+                value={name}
+                onChange={handleNameChange}
+                errorFeedback={getErrorFeedback('name')}
               />
 
               <Input
@@ -47,24 +141,36 @@ export const SignUp: React.FC = () => {
                 name='email'
                 type='email'
                 placeholder='exemplo@gmail.com'
+                value={email}
+                onChange={handleEmailChange}
+                errorFeedback={getErrorFeedback('email')}
               />
 
               <Input
                 label='Telefone'
                 name='phone'
                 type='text'
+                value={phone}
+                onChange={handlePhoneChange}
+                errorFeedback={getErrorFeedback('phone')}
               />
 
               <PasswordInput
                 label='Senha'
                 name='password'
                 placeholder='Sua senha aqui'
+                value={password}
+                onChange={handlePasswordChange}
+                errorFeedback={getErrorFeedback('password')}
               />
 
               <PasswordInput
                 label='Confirmar senha'
                 name='confirmPassword'
                 placeholder='Confirme sua senha aqui'
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                errorFeedback={getErrorFeedback('confirmPassword')}
               />
             </div>
 
