781a2b2709ca700816101806013f309536e54bca
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-20 21:03:42 -0300

    feat: added auth service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,23 +1,38 @@
+import { api } from './api.service';
+import * as Auth from '../types/AuthDTO';
+
 export class AuthService {
 
-  static async logIn() {
+  static async logIn({ email, password, rememberMe }: Auth.LogInRequest): Promise<Auth.LogInResponse> {
+    const logInResponse = await api.post('/auth/signin', { email, password, rememberMe }) as Auth.LogInResponse;
 
+    return logInResponse;
   }
 
-  static async signUp() {
+  static async signUp({
+    name, email, phone, password, confirmPassword, notifyServicesByEmail, acceptedTermsAndConditions,
+  }: Auth.SignUpRequest): Promise<Auth.LogInResponse> {
+    const signUpResponse = await api.post('/auth/signup', {
+      name, email, phone, password, confirmPassword, notifyServicesByEmail, acceptedTermsAndConditions,
+    }) as Auth.LogInResponse;
 
+    return signUpResponse;
   }
 
-  static async signOut() {
-
+  static async signOut({ userId, tokenRequesting }: Auth.SignOutRequest): Promise<void> {
+    await api.post('/auth/signout', { userId, tokenRequesting });
   }
 
-  static async verifyToken() {
+  static async verifyToken({ userId, tokenRequesting }: Auth.VerifyTokenRequest): Promise<Auth.VerifyTokenResponse> {
+    const verifyTokenResponse = await api.post('/auth/validate', { userId, tokenRequesting }) as Auth.VerifyTokenResponse;
 
+    return verifyTokenResponse;
   }
 
-  static async generateNewToken() {
+  static async generateNewToken({ refreshToken }: Auth.GenerateNewTokenRequest): Promise<Auth.GenerateNewTokenResponse> {
+    const generateNewTokenResponse = await api.post('/auth/refresh-token', { refreshToken }) as Auth.GenerateNewTokenResponse;
 
+    return generateNewTokenResponse;
   }
 
 }
--- src/types/AuthDTO.ts
+++ src/types/AuthDTO.ts
@@ -1,6 +1,6 @@
 import { User } from './User';
 
-export interface LoginInRequest {
+export interface LogInRequest {
   email: string;
   password: string;
   rememberMe: boolean;
