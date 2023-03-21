350bcf59f6de36c23b0cebb0be89d210d16a0e5d
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-20 23:18:49 -0300

    feat: applied custom routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,38 @@
-import { Route, Routes as ReactRoutes } from 'react-router-dom';
+import { Routes as ReactRoutes } from 'react-router-dom';
+import { CustomRoute } from './components/shared/CustomRoute';
 import { HomePage, NotFoundPage, PricesPage, SignInPage, SignUpPage } from './pages';
 
 export function Routes() {
   return (
     <ReactRoutes>
-      <Route path='/' element={<HomePage />} />
+      <CustomRoute
+        type='public'
+        path='/'
+        element={<HomePage />}
+      />
 
-      <Route path='/prices' element={<PricesPage />} />
+      <CustomRoute
+        type='public'
+        path='/prices'
+        element={<PricesPage />}
+      />
 
-      <Route path='/login' element={<SignInPage />} />
-      <Route path='/signup' element={<SignUpPage />} />
+      <CustomRoute
+        type='not_authenticated'
+        path='/login'
+        element={<SignInPage />}
+      />
 
-      <Route path='*' element={<NotFoundPage />} />
+      <CustomRoute
+        type='not_authenticated'
+        path='/signup'
+        element={<SignUpPage />}
+      />
+
+      <CustomRoute
+        path='*'
+        element={<NotFoundPage />}
+      />
     </ReactRoutes>
   );
 }
