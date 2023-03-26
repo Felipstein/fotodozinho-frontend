c3b2960d889e534bdf0e9a6e941c13382b1a1da3
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-26 11:49:45 -0300

    feat: finished validate email page

--- src/pages/ValidateEmail/index.tsx
+++ src/pages/ValidateEmail/index.tsx
@@ -1,24 +1,47 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import { Logo } from '../../components/common/Logo';
 import { SplashScreen } from '../../components/layout/SplashScreen';
+import { APIError } from '../../errors/APIError';
+import { ValidateEmailService } from '../../services/validate-email.service';
+
+import * as S from './styles';
 
 export const ValidateEmail: React.FC = () => {
+  const navigate = useNavigate();
   const { validateToken } = useParams();
 
   useEffect(() => {
     async function validateEmail() {
 
+      try {
+        if(!validateToken) {
+          throw new APIError('Token inválido', -1);
+        }
 
+        await ValidateEmailService.validateEmail({ token: validateToken });
 
+        toast.success('Seu e-mail agora está validado. Agora você pode acessar nossos serviços!');
+      } catch (err: APIError | Error | any) {
+        toast.error(err.message || 'Ocorreu um problema ao validar seu e-mail, tente novamente.');
+
+        navigate('/verifyrequest');
+      }
     }
 
     validateEmail();
-  }, []);
+  }, [validateToken, navigate]);
 
   return (
-    <SplashScreen
-      visible
-      loadingMessage='Estamos validando seu e-mail...'
-    />
+    <S.CenteredContent>
+      <SplashScreen
+        visible
+        loadingMessage='Estamos validando seu e-mail...'
+      />
+
+      <Logo />
+    </S.CenteredContent>
   );
 };
---
+++ src/pages/ValidateEmail/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const CenteredContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
+`;
