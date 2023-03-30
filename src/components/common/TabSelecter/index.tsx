a501fb79ecd04d7959731bdacd536f615c535a52
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-30 15:26:04 -0300

    feat: add on click prop to tab selecter

--- src/components/common/TabSelecter/index.tsx
+++ src/components/common/TabSelecter/index.tsx
@@ -1,13 +1,14 @@
 import { TabSelecterProps } from './types';
 import * as S from './styles';
 
-export const TabSelecter: React.FC<TabSelecterProps> = ({ isSelected = false, children }) => {
+export const TabSelecter: React.FC<TabSelecterProps> = ({ isSelected = false, onClick, children }) => {
 
   return (
     <S.Container
       role='button'
       tabIndex={0}
       isSelected={isSelected}
+      onClick={onClick}
     >
       {children}
     </S.Container>
--- src/components/common/TabSelecter/types.ts
+++ src/components/common/TabSelecter/types.ts
@@ -1,4 +1,5 @@
 export interface TabSelecterProps {
   isSelected?: boolean;
+  onClick?: () => void;
   children: string;
 }
