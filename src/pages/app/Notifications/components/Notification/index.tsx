95012004bbc38f949384945c3b6bbd7a6ad7f3c7
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-30 12:32:48 -0300

    style: style notification

--- src/pages/app/Notifications/components/Notification/index.tsx
+++ src/pages/app/Notifications/components/Notification/index.tsx
@@ -1,5 +1,9 @@
 import { NotificationProps } from './types';
 import * as S from './styles';
+import { InfoIcon } from '../../../../../icons/InfoIcon';
+import { Text } from '../../../../../components/common/Text';
+import { IconButton } from '../../../../../components/common/IconButton';
+import { WarnIcon } from '../../../../../icons/WarnIcon';
 
 export const Notification: React.FC<NotificationProps> = ({
   id,
@@ -11,7 +15,39 @@ export const Notification: React.FC<NotificationProps> = ({
 
   return (
     <S.Container>
-      <h1>Notificação</h1>
+      <div className="data-info">
+        <InfoIcon size={24} />
+
+        <div className="data-time">
+          <Text size='xsm'>
+            08/01/2022
+          </Text>
+
+          <Text size='xsm'>
+            10:29
+          </Text>
+        </div>
+      </div>
+
+      <main className="content">
+        <Text className='notification-title'>
+          Pedido realizado com sucesso!
+        </Text>
+
+        <Text className='notification-body' size='sm'>
+          Agora só falta escolher o método de pagamento
+        </Text>
+      </main>
+
+      <div className="actions">
+        <IconButton>
+          <InfoIcon />
+        </IconButton>
+
+        <IconButton>
+          <WarnIcon />
+        </IconButton>
+      </div>
     </S.Container>
   );
 };
--- src/pages/app/Notifications/components/Notification/styles.ts
+++ src/pages/app/Notifications/components/Notification/styles.ts
@@ -1,3 +1,56 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1.2rem;
+
+  padding: 1.6rem 0.8rem;
+  width: 100%;
+
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[200]};
+
+  .data-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    color: ${({ theme }) => theme.colors.gray[300]};
+
+    .data-time {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      span {
+        font-weight: 500;
+      }
+    }
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+
+    flex: 1;
+
+    .notification-title {
+      color: ${({ theme }) => theme.colors.gray[900]};
+      font-weight: 500;
+    }
+
+    .notification-body {
+      color: ${({ theme }) => theme.colors.gray[600]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+    gap: 0.4rem;
+
+    color: ${({ theme }) => theme.colors.gray[900]};
+  }
+`;
